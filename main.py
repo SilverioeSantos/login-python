2996bbe0fd6ad036d289fbac106225cf39cc597c
Lucas Silverio <lucassilverioesantos@gmail.com>
2025-03-31 21:11:30 -0300

    janela-principal

---
+++ main.py
@@ -0,0 +1,16 @@
+import customtkinter as ctk
+
+# Configuração aparência
+ctk.set_appearance_mode("dark")
+
+# Criação da janela principal
+app = ctk.CTk()
+app.title("Sistema de Login")
+app.geometry("400x300")
+
+# Criação dos campos
+
+# Criação das funções de funcionalidades
+
+# Iniciar a aplicação
+app.mainloop()
