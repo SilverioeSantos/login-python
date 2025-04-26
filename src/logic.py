88ca544c11b6ad65f84936fc99f3d97b957d3f10
Lucas Silverio <lucassilverioesantos@gmail.com>
2025-04-25 22:06:53 -0300

    Update-test-pytest

--- src/logic.py
+++ src/logic.py
@@ -1,4 +1,5 @@
-# logic.py
+"""Módulo de lógica para validação de credenciais."""
+
 def validar_credenciais(usuario, senha):
     """Função que valida as credenciais do usuário."""
     return usuario == "LucasSilverio" and senha == "123456"
